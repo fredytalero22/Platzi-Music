e99086cc875662019caa9ea8c7c6103d88d249cd
Fredy Talero <fredy.talero@gmail.com>
2020-05-19 17:34:51 -0500

    Primer commit, clase numero 18

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar canciones",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{searchMessage}}
+      .container
+        .columns
+          .column(v-for="t in tracks") {{t.name}} - {{t.artist}}
+</template>
+
+<script>
+const tracks = [
+  {name: 'Muchacha', artist: 'Luis Alberto Spinetta'},
+  {name: 'Hoy aca en el baile', artist: 'El pepo'},
+  {name: 'I was made for loving', artist: 'Kiss'}
+]
+export default {
+  name: 'app',
+  data () {
+    return {
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  methods:{
+    search(){
+      console.log(this.searchQuery);
+      this.tracks = tracks;
+    }
+  },
+
+  computed: {
+    searchMessage(){
+      return `Encontrados: ${this.tracks.length}`
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
