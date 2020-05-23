31c890e23f7d8a86f12d7d76a632e13a58779b59
Fredy Talero <fredy.talero@gmail.com>
2020-05-23 12:38:39 -0500

    Se crean los compoenente Header y Footer y se importan de manera local al App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    pm-header
     section.section
       nav.nav.has-shadow
         .container
@@ -17,10 +18,13 @@
         .columns
           .column(v-for="t in tracks")
             | {{t.name}} - {{t.artists[0].name}}
+    pm-footer
 </template>
 
 <script>
 import trackService from './services/track'
+import PmHeader from './components/layout/Header.vue'
+import PmFooter from './components/layout/Footer.vue'
 
 export default {
   name: 'app',
@@ -29,6 +33,11 @@ export default {
       searchQuery: '',
       tracks: []
     }
+  },
+
+  components: {
+    PmFooter,
+    PmHeader
   },
 
   methods: {
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+    footer.footer
+        .container
+            .content.has-text-centered
+                p 💻 with 💙 by @fredytalero22
+</template>
