b384bfa7d5147f3bfeaaed93aa78564bf4735330
Fredy Talero <fredy.talero@gmail.com>
2020-05-25 11:37:17 -0500

    Fin del modulo Vue Router

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-    .card
+    .card(v-if="track && track.album")
         .car-image
             figure.image.is-1by1
                 img(:src="track.album.images[0].url")
@@ -18,6 +18,8 @@
                         .level-left
                             a.level-item
                                 span.icon.is-small(@click="selectTrack") ▶
+                            a.level-item
+                                span.icon.is-small(@click="goToTrack(track.id)") 🔎
 </template>
 
 <script>
@@ -31,7 +33,12 @@ export default {
       this.$emit('select', this.track.id)
 
       this.$bus.$emit('set-track', this.track)
+    },
+
+    goToTrack (id) {
+      this.$router.push({ name: 'track', params: { id } })
     }
+
   }
 }
 
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -7,6 +7,8 @@
                         .nav-item
                             strong 🎶 Platzi Music
                     .nav-rigth.nav-menu
+                        router-link.nav-item(:to="{ name: 'search'}") Buscar
+                        router-link.nav-item(to="about") Nosotros
         .hero-body
             .container.has-text-centered
                 h1.title Platzi Music
