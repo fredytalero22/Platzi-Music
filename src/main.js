5b915c000d0e386bcbe946afe34b3c0689756cb5
Fredy Talero <fredy.talero@gmail.com>
2020-06-03 21:09:07 -0500

    Filters ms-to-mm

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from '@/App.vue'
 
 import routes from '@/routes'
 import EventBus from '@/plugins/event-bus'
+import msToMm from '@/filters/ms-to-mm'
 
 Vue.use(VueRouter)
 Vue.use(EventBus)
+Vue.use(msToMm)
 
 const router = new VueRouter({ routes })
 
